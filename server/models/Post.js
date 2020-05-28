b455cb68dcd51098afb24fe9d929b7b9cf4b9409
Roy <roymeplease@gmail.com>
2020-05-28 17:11:41 +0300

    the file name fix from Posts to Post.

---
+++ server/models/Post.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    created   : {
+		type    : Date,
+		default : Date.now
+	},
+    content: String,
+    image: String,
+    video: String,
+    likes: String,
+    user: {
+        type: String,
+        required: true,
+    } 
+})
+
+const Post = mongoose.model('Post', PostSchema);
+
+module.exports = Post;
