f02e21dd87f6f081225110f16282155564ee2f96
Roy <roymeplease@gmail.com>
2020-06-06 20:03:31 +0300

    comment likes. user can now like a comment

--- server/routes/likes.js
+++ server/routes/likes.js
@@ -1,4 +1,12 @@
-const { getLikes, addLike, removeLike } = require('../services/like-services');
+const { getPostLikes,
+	getCommentLikes,
+	addLikeToPost,
+	addLikeToComment,
+	removeLikeFromPost,
+	removeLikeFromComment
+} = require('../services/like-services');
+
+const Comment = require('../models/Comment')
 
 const verifyUser = require('../services/auth-services');
 
@@ -6,7 +14,7 @@ module.exports = function(app) {
 	// get all likes of a specific post
 	app.get('/api/posts/:postId/likes', verifyUser, async (req, res) => {
 		try {
-			const likes = await getLikes(req.params.postId);
+			const likes = await getPostLikes(req.params.postId);
 			res.status(200).json(likes).end();
 		} catch (error) {
 			res
@@ -16,10 +24,10 @@ module.exports = function(app) {
 		}
 	});
 
-	// Add likes to a post
+	// Add likes of a post
 	app.post('/api/posts/:postId/likes', verifyUser, async (req, res) => {
 		try {
-			const like = await addLike({post: req.params.postId, user: req.user.sub});
+			const like = await addLikeToPost({post: req.params.postId, user: req.user.sub});
 			res.status(200).json(like).end();
 		} catch (error) {
 			res
@@ -29,10 +37,54 @@ module.exports = function(app) {
 		}
 	});
 
-	// Remove like
+	// Remove like from a post
 	app.delete('/api/posts/:postId/likes/:likeId', verifyUser, async (req, res) => {
 		try {
-			const like = await removeLike(req.params.postId);
+			const like = await removeLikeFromPost(req.params.postId);
+			res.status(200).json(like).end();
+		} catch (error) {
+			res
+				.status(500)
+				.json({
+					message : `internal error while trying to remove a like`
+				})
+				.end();
+		}
+	});
+
+
+	// Get likes of a comment
+	app.get('/api/comments/:commentId/likes', verifyUser, async (req, res) => {
+		try {
+			const likes = await getCommentLikes(req.params.commentId);
+			res.status(200).json(likes).end();
+		} catch (error) {
+			res
+				.status(500)
+				.json({ message: `internal error while trying to get likes` })
+				.end();
+		}
+	});
+
+	// Add likes to a comment
+	app.post('/api/comments/:commentId/likes', verifyUser, async (req, res) => {
+		try {
+			const comment = await Comment.findOne({_id: req.params.commentId})
+			const like = await addLikeToComment({ user: req.user.sub, comment: req.params.commentId, post: comment.post});
+			res.status(200).json(like).end();
+		} catch (error) {
+			console.log(error);
+			res
+				.status(500)
+				.json({ message: `internal error while trying to add a like` })
+				.end();
+		}
+	});
+
+	// Remove like from a comment
+	app.delete('/api/comments/:commentId/likes', verifyUser, async (req, res) => {
+		try {
+			const like = await removeLikeFromComment(req.params.commentId);
 			res.status(200).json(like).end();
 		} catch (error) {
 			res
