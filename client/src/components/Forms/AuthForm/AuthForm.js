2939ab0ec252420dd431cef454d74d9e542d857c
Omer Gronich <gronichomer@gmail.com>
2020-06-12 18:13:36 +0300

    started work on auth switch

--- client/src/components/Forms/AuthForm/AuthForm.js
+++ client/src/components/Forms/AuthForm/AuthForm.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Signup from '../SignupForm/SignupForm';
 import LoginForm from '../LoginForm/LoginForm';
+import AuthSwitch from '../AuthSwitch/AuthSwitch';
 
 const AuthForm = () => {
+	const [
+		hasAccount,
+		setHasAccount
+	] = useState(true);
+
 	return (
 		<div>
 			<Signup />
 			{/* <LoginForm /> */}
+			<AuthSwitch hasAccount={hasAccount} setHasAccount={setHasAccount} />
 		</div>
 	);
 };
---
+++ client/src/components/Forms/AuthSwitch/AuthSwitch.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './AuthSwitch.module.scss';
+
+const AuthSwitch = ({ hasAccount, setHasAccount }) => {
+	const hasAccountText = !hasAccount
+		? "Don't have an account?"
+		: 'Have an account?';
+	const linkText = !hasAccount ? 'Sign up' : 'Log in';
+
+	return (
+		<div className={styles.wrapper}>
+			<p className={styles.paragraph}>
+				{hasAccountText}
+				<a href='#' className={styles.link}>
+					<span
+						className={styles.linkText}
+						onClick={() => setHasAccount(!hasAccount)}
+					>
+						{linkText}
+					</span>
+				</a>
+			</p>
+		</div>
+	);
+};
+
+export default AuthSwitch;
