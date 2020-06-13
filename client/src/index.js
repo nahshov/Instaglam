02cfc694eb061ba9df86f381ce1c7f6f35ec062c
Omer Gronich <gronichomer@gmail.com>
2020-06-14 00:34:47 +0300

    commented store import in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from './app/store';
+// import store from './app/store';
 // import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-
 	<React.StrictMode>
 		<BrowserRouter>
-		{/* <Provider store={store}> */}
-		<App />
-		{/* </Provider> */}
+			{/* <Provider store={store}> */}
+			<App />
+			{/* </Provider> */}
 		</BrowserRouter>
+		//{' '}
 	</React.StrictMode>,
 	document.getElementById('root')
 );
