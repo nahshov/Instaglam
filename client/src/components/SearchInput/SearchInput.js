d1c2109372ed04bd4e83aa1bd5735701d990f501
almog <almoghr36@gmail.com>
2020-06-14 16:53:12 +0300

    finished working on the css design of the search input in the homepage

--- client/src/components/SearchInput/SearchInput.js
+++ client/src/components/SearchInput/SearchInput.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import styles from './SearchInput.module.scss';
 import { AiOutlineSearch } from 'react-icons/ai';
+import { TiDelete } from 'react-icons/ti';
 
 const SearchInput = () => {
 	return (
-		<div className={styles.SearchInputContainer}>
+		<div className={styles.searchInputContainer}>
 			<input className={styles.SearchInput} />
-			<label className={styles.SearchPlaceholder}>
-				<AiOutlineSearch />Search
+			<label className={styles.searchLabel}>
+                <div>
+				    <AiOutlineSearch className={styles.searchIcon}/> <span>Search</span>
+                </div>
+                <TiDelete className={styles.deleteIcon} />
 			</label>
 		</div>
 	);
