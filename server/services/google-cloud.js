37d53f69489fe71942df3ae01d1a91fbf0dc2b42
roi <roymeplease@gmail.com>
2020-06-14 18:52:16 +0300

    added bucket name to .env

--- server/services/google-cloud.js
+++ server/services/google-cloud.js
@@ -1,6 +1,5 @@
 const { Storage } = require('@google-cloud/storage');
 const path = require('path');
-const bucketName = 'instaglam-bucket';
 require('dotenv').config();
 
 
@@ -8,10 +7,10 @@ const gc = new Storage({
 	keyFilename : path.join(__dirname, '../../dev1-263221-7258d8ee70b0.json'),
 	projectId   : process.env.PROJECT_ID
 });
-const bucket = gc.bucket('instaglam-bucket');
-
+const bucket = gc.bucket(process.env.BUCKET_NAME);
+console.log(bucket)
 const deleteFromBucket = async url => {
-	const fileName = url.split(`${bucketName}/`)[1];
+	const fileName = url.split(`${process.env.BUCKET_NAME}/`)[1];
 	return bucket.file(fileName).delete();
 };
 
