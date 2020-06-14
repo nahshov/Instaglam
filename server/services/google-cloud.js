572bbbf5344198ec537a25893f76b517c62bd17a
roi <roymeplease@gmail.com>
2020-06-14 16:29:28 +0300

    dotenv installation and .env file wit the PROJECT_ID env variable

--- server/services/google-cloud.js
+++ server/services/google-cloud.js
@@ -1,10 +1,12 @@
 const { Storage } = require('@google-cloud/storage');
 const path = require('path');
 const bucketName = 'instaglam-bucket';
+require('dotenv').config();
+
 
 const gc = new Storage({
 	keyFilename : path.join(__dirname, '../../dev1-263221-7258d8ee70b0.json'),
-	projectId   : 'dev1-263221'
+	projectId   : process.env.PROJECT_ID
 });
 const bucket = gc.bucket('instaglam-bucket');
 
