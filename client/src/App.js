57149c42078e0ea04239e8087a3b88404ff042b2
Omer Gronich <gronichomer@gmail.com>
2020-06-16 11:17:54 +0300

    created color-palette.scss to hold variables for all colors

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
+import './App.scss';
 import AuthForm from './components/Forms/AuthForm/AuthForm';
 import Navbar from './components/Navbar/Navbar';
 import { Route, Switch } from 'react-router-dom';
