1c9b33c2030d513111fdcfc01b90fbfe85cf4836
Omer Gronich <gronichomer@gmail.com>
2020-06-16 12:15:47 +0300

    added new line to the end of .gitignore

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'index.scss';
 import App from 'App';
-// import store from './app/store';
-// import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <Provider store={store}> */}
       <App />
-      {/* </Provider> */}
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
