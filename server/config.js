6e436fe8056e6d16f34b729ddfdd1f66437d1f7f
Omer Gronich <gronichomer@gmail.com>
2020-06-22 00:10:29 +0300

    changed port to 4000

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/instaglam',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 4000,
   tokenSecret: process.env.TOKEN_SECRET || 'mySecret',
   refreshTokenSecret: process.env.REFRESH_TOKEN || 'myRefresh',
   googleStorageCredentials: {
