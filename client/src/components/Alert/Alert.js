cb8574429248db5f70f9996a4677189f2942fce9
Roy <roymeplease@gmail.com>
2020-06-28 14:21:23 +0300

    form alerts

--- client/src/components/Alert/Alert.js
+++ client/src/components/Alert/Alert.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './Alert.module.scss';
 
 const Alert = ({ alerts }) => (
-  // alerts.length > 0 && alerts
   <div className={styles.msgDiv}>
     <p>{alerts}</p>
   </div>
