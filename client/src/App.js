7f181c8dac30259c2a90664152141af1c45eb667
Omer Gronich <gronichomer@gmail.com>
2020-06-29 17:44:56 +0300

    fixed order of routes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -22,18 +22,18 @@ const App = () => {
           <Switch>
             <Route path="/accounts/emailsignup" exact component={SignUpForm} />
             <Route path="/accounts/login" exact component={LogInForm} />
-            <ProtectedRoute exact path="/:profile" component={ProfilePage} />
-            <ProtectedRoute exact path="/" component={HomePage} />
             <ProtectedRoute
               exact
               path="/explore"
               component={() => <div>Explore!</div>}
             />
+            <ProtectedRoute exact path="/:profile" component={ProfilePage} />
             <ProtectedRoute
               exact
               path="/direct/inbox"
               component={() => <div>Chat!</div>}
             />
+            <ProtectedRoute exact path="/" component={HomePage} />
           </Switch>
         </div>
       </Router>
