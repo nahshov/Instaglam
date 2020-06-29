adf2ccda5dc1b884bb2af9c98331ba38db590187
Omer Gronich <gronichomer@gmail.com>
2020-06-29 17:16:34 +0300

    fixed proptypes

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styles from 'components/Navbar/Navbar.module.scss';
 import Logo from 'components/Logo/Logo';
@@ -16,5 +17,19 @@ const Navbar = ({ history }) => (
     </div>
   </nav>
 );
+
+Navbar.propTypes = {
+  history: PropTypes.shape({
+    length: PropTypes.number.isRequired,
+    action: PropTypes.string.isRequired,
+    location: PropTypes.object.isRequired,
+    push: PropTypes.func.isRequired,
+    replace: PropTypes.func.isRequired,
+    go: PropTypes.func.isRequired,
+    goBack: PropTypes.func.isRequired,
+    goForward: PropTypes.func.isRequired,
+    block: PropTypes.func.isRequired
+  }).isRequired
+};
 
 export default Navbar;
--- client/src/components/Popover/Popover.js
+++ client/src/components/Popover/Popover.js
@@ -1,3 +1,4 @@
+/* eslint-disable implicit-arrow-linebreak */
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Popover.module.scss';
