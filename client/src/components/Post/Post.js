e991bbdd11868e0c1c9df30a4249f6048fd0cbc6
almog <almoghr36@gmail.com>
2020-07-02 20:26:53 +0300

    added header, picture area and icons to the post assigment. need to do: comments area, likes area, post a comment area and offcourse working on the react features

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProfilePic from 'components/ProfilePic/ProfilePic';
+import { FiMoreHorizontal } from 'react-icons/fi';
+import { RiChat3Line } from 'react-icons/ri';
+import { GrBookmark } from 'react-icons/gr';
+import HeartIcon from 'components/Icons/HeartIcon/HeartIcon';
+import ShareModalIcon from 'components/Icons/ChatIcon/ChatIcon';
 import styles from './Post.module.scss';
 
-const Post = () => (
-  <article className={styles.postContainer}>
-    <div className={styles.postHeader}>
-      <div className={styles.userIdentifier}>
-        <ProfilePic />
+const Post = () => {
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  return (
+    <article className={styles.postContainer}>
+      <header className={styles.headerPostContainer}>
+        <div className={styles.postHeader}>
+          <div className={styles.userIdentifier}>
+            <div className={styles.circle} />
+            <a href="#" className={styles.username}>
+              name
+            </a>
+          </div>
+          <FiMoreHorizontal className={styles.moreIcon} onClick={() => console.log('hello')} />
+        </div>
+      </header>
+      <div className={styles.postPicture} />
+      <div className={styles.iconsWrapper}>
+        <div className={styles.leftIconsWrapper}>
+          <HeartIcon className={styles.heartIcon} />
+          <RiChat3Line className={styles.chatIcon} />
+          <ShareModalIcon className={styles.ShareModalIcon} />
+        </div>
+        <GrBookmark className={styles.bookMark} />
       </div>
-    </div>
-  </article>
-);
+    </article>
+  );
+};
 
 export default Post;
