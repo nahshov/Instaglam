bedcb9681e2c4d19a8784afb271e043c276b7afa
Omer Gronich <gronichomer@gmail.com>
2020-07-04 18:48:57 +0300

    overlay now shows on hover

--- client/src/pages/ProfilePage/ProfilePage.js
+++ client/src/pages/ProfilePage/ProfilePage.js
@@ -4,7 +4,11 @@ import { Redirect, useLocation, Link } from 'react-router-dom';
 import styles from 'pages/ProfilePage/ProfilePage.module.scss';
 import ProfilePic from 'components/ProfilePic/ProfilePic';
 import Button from 'components/Button/Button';
-import { logout, uploadProfilePic, removeProfilePic } from 'actions/auth/authActions';
+import {
+  logout,
+  uploadProfilePic,
+  removeProfilePic
+} from 'actions/auth/authActions';
 import { setAlert } from 'actions/alerts/alertActions';
 import { searchUser } from 'actions/users/userActions';
 import { loadPostsOfUser } from 'actions/posts/postActions';
@@ -18,10 +22,10 @@ import { BsChatFill } from 'react-icons/bs';
 const ProfilePage = () => {
   const {
     auth: { user: authenticatedUser, isAuthenticated, loading: authLoading },
-    users: { user, loading: userLoading },
-    posts: { postsOfUser, loading: postsLoading },
+    users: { user: searchedUser, loading: userLoading },
+    posts: { postsOfUser: postsOfSearchedUser, loading: postsLoading },
     alert: { message }
-  } = useSelector((state) => state);
+  } = useSelector(state => state);
 
   const [isSettingsModalOpen, setSettingsModalOpen] = useState(false);
 
@@ -29,31 +33,33 @@ const ProfilePage = () => {
 
   const { pathname } = useLocation();
 
-  const username = pathname.split('/')[1];
+  const searchedUserUsername = pathname.split('/')[1];
 
   useEffect(() => {
-    dispatch(searchUser(username));
-  }, [username]);
+    dispatch(searchUser(searchedUserUsername));
+  }, [searchedUserUsername]);
 
   useEffect(() => {
-    if (user) {
-      dispatch(loadPostsOfUser(user._id));
+    if (searchedUser) {
+      dispatch(loadPostsOfUser(searchedUser._id));
     }
-  }, [user]);
+  }, [searchedUser]);
 
   if (!isAuthenticated && !authLoading) {
     return <Redirect to="/accounts/login" />;
   }
 
   const toggleProfilePicModal = () => {
-    if (username === authenticatedUser.username) {
+    if (searchedUserUsername === authenticatedUser.username) {
       setSettingsModalOpen(!isSettingsModalOpen);
     }
   };
 
-  const handleSelectedFile = (e) => {
+  const handleSelectedFile = e => {
     if (e.target.files[0].size > 1000000) {
-      dispatch(setAlert('The maximum size for a profile picture is 1mb', 'Error'));
+      dispatch(
+        setAlert('The maximum size for a profile picture is 1mb', 'Error')
+      );
       setTimeout(() => {
         dispatch(setAlert('', null));
       }, 4500);
@@ -70,14 +76,23 @@ const ProfilePage = () => {
 
   return (
     <main className={styles.main}>
-      <Modal isOpen={isSettingsModalOpen} setSettingsModalOpen={setSettingsModalOpen}>
+      <Modal
+        isOpen={isSettingsModalOpen}
+        setSettingsModalOpen={setSettingsModalOpen}
+      >
         <ModalList>
           <h3>Change Profile Photo</h3>
           <ModalListItem>
             <label htmlFor="profilePic" className={styles.uploadPhoto}>
               Upload Photo
             </label>
-            <input type="file" id="profilePic" name="profilePic" onChange={handleSelectedFile} style={{ display: 'none' }} />
+            <input
+              type="file"
+              id="profilePic"
+              name="profilePic"
+              onChange={handleSelectedFile}
+              style={{ display: 'none' }}
+            />
           </ModalListItem>
           <ModalListItem>
             <Button
@@ -104,17 +119,17 @@ const ProfilePage = () => {
               onClick={toggleProfilePicModal}
             >
               {!userLoading && (
-              <ProfilePic
-                url={user.profilePic}
-                className={styles.profilePic}
-              />
+                <ProfilePic
+                  url={searchedUser.profilePic}
+                  className={styles.profilePic}
+                />
               )}
             </button>
             <Alert alerts={message} style={{ fontSize: '10px' }} />
           </div>
           <section className={styles.profileInfo}>
             <div className={styles.profileInfoHeader}>
-              <h2 className={styles.username}>{user.username}</h2>
+              <h2 className={styles.username}>{searchedUser.username}</h2>
               <Link className={styles.editLink} to="/accounts/edit">
                 <Button text="Edit Profile" />
               </Link>
@@ -127,7 +142,9 @@ const ProfilePage = () => {
             </div>
             <ul className={styles.socialStatusList}>
               <li>
-                <span>{postsOfUser && `${postsOfUser.length} `}</span>
+                <span>
+                  {postsOfSearchedUser && `${postsOfSearchedUser.length} `}
+                </span>
                 posts
               </li>
               <li>
@@ -140,36 +157,45 @@ const ProfilePage = () => {
               </li>
             </ul>
             <div className={styles.bioContainer}>
-              <h1 className={styles.fullName}>{user.fullName}</h1>
-              <p className={styles.bio}>{user.bio}</p>
+              <h1 className={styles.fullName}>{searchedUser.fullName}</h1>
+              <p className={styles.bio}>{searchedUser.bio}</p>
             </div>
           </section>
         </header>
-        {!postsOfUser.length && !postsLoading
-          ? (
-            <div className={styles.noPostsUploaded}>
-              <h2>No posts uploaded yet...</h2>
-            </div>
-          ) : (
-            <div className={styles.profilePostsContainer}>
-              {postsOfUser.map(post => (
-                <div key={post._id} className={styles.profilePost} style={{ background: `url(${post.media}) no-repeat center center / cover` }}>
-                  <div className={styles.profilePostOverlay}>
-                    <div className={styles.profilePostIconsContainer}>
-                      <div className={styles.profilePostLikes}>
-                        <AiFillHeart className={styles.profilePostLikeIcon} />
-                        <span className={styles.profilePostNumOfLikes}>{post.likes}</span>
-                      </div>
-                      <div className={styles.profilePostComments}>
-                        <BsChatFill className={styles.profilePostCommentsIcon} />
-                        <span className={styles.profilePostNumOfComments}>{post.comments}</span>
-                      </div>
+        {!postsOfSearchedUser.length && !postsLoading ? (
+          <div className={styles.noPostsUploaded}>
+            <h2>No posts uploaded yet...</h2>
+          </div>
+        ) : (
+          <div className={styles.profilePostsContainer}>
+            {postsOfSearchedUser.map(post => (
+              <div
+                key={post._id}
+                className={styles.profilePost}
+                style={{
+                  background: `url(${post.media}) no-repeat center center / cover`
+                }}
+              >
+                <div className={styles.profilePostOverlay}>
+                  <div className={styles.profilePostIconsContainer}>
+                    <div className={styles.profilePostLikes}>
+                      <AiFillHeart className={styles.profilePostLikeIcon} />
+                      <span className={styles.profilePostNumOfLikes}>
+                        {post.likes}
+                      </span>
+                    </div>
+                    <div className={styles.profilePostComments}>
+                      <BsChatFill className={styles.profilePostCommentsIcon} />
+                      <span className={styles.profilePostNumOfComments}>
+                        {post.comments}
+                      </span>
                     </div>
                   </div>
                 </div>
-              ))}
-            </div>
-          ) }
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </main>
   );
