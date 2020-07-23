2d58dd5233d06a7e176ad876f8df536147948240
Omer Gronich <gronichomer@gmail.com>
2020-07-23 07:03:06 +0300

    changed heroku-postbuild

--- server/server.js
+++ server/server.js
@@ -24,10 +24,10 @@ app.use((err, req, res, next) => {
 require('./routes/index.js')(app);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
   });
 }
 
